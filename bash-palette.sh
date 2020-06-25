f5d41eb602e53e62af97cfcafcb7d5a940254c39
Ferro Mauro <ferromauro@yahoo.it>
2020-06-25 18:36:10 +0200

    Initial commit

---
+++ bash-palette.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+# Author: Mauro Ferro
+# GitHub: https://github.com/ferromauro/bash-palette
+# Normal Text
+
+export PALETTE_RESET='\e[30;0;40m'
+
+export PALETTE_BLACK='\e[30m'
+export PALETTE_RED='\e[31m'
+export PALETTE_GREEN='\e[32m'
+export PALETTE_BROWN='\e[33m'
+export PALETTE_BLUE='\e[34m'
+export PALETTE_PURPLE='\e[35m'
+export PALETTE_CYAN='\e[36m'
+export PALETTE_LIGHTGRAY='\e[37m'
+# Bold Text
+export PALETTE_BLACK_B='\e[1;30m'
+export PALETTE_RED_B='\e[31;1m'
+export PALETTE_GREEN_B='\e[1;32m'
+export PALETTE_BROWN_B='\e[1;33m'
+export PALETTE_BLUE_B='\e[1;34m'
+export PALETTE_PURPLE_B='\e[1,35m'
+export PALETTE_CYAN_B='\e[1;36m'
+export PALETTE_LIGHTGRAY_B='\e[1:37m'
+
+
