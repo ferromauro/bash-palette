10c32d6005a65579d639cf9992096a3a2114b16b
root <ferromauro@yahoo.it>
2020-06-26 00:01:41 +0000

    Colors

---
+++ test.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+source bash-palette.sh
+echo -e ${PALETTE_BLU}Colore BLUE
+echo -e ${PALETTE_RED_B}Colore RED Bold
+echo -e ${PALETTE_GREEN_B}Colore GREEN
+echo -e ${PALETTE_CYAN_B}Colore CYAN BOLD
+echo -e ${PALETTE_RESET}Colore NORMAL
+
+
+
