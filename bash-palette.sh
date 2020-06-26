ff276bb95c06500b3e89cb0c5871008f675fb614
StartG17 <ferromauro@yahoo.it>
2020-06-26 09:42:18 +0200

    Version 0.2

--- bash-palette.sh
+++ bash-palette.sh
@@ -1,10 +1,23 @@
 #! /bin/bash
 # Author: Mauro Ferro
 # GitHub: https://github.com/ferromauro/bash-palette
+
 # Normal Text
+export PALETTE_RESET='\e[0m'
 
-export PALETTE_RESET='\e[30;0;40m'
+# Bold Text
+export PALETTE_BOLD='\e[1m'
 
+# Underlined Text
+export PALETTE_UNDERLINED='\e[4m'
+
+# Blinking
+export PALETTE_BLINK='\e[5m'
+
+# Reverse
+export PALETTE_REVERSE='\e[7m'
+
+# Foreground Color
 export PALETTE_BLACK='\e[30m'
 export PALETTE_RED='\e[31m'
 export PALETTE_GREEN='\e[32m'
@@ -13,23 +26,16 @@ export PALETTE_BLUE='\e[34m'
 export PALETTE_PURPLE='\e[35m'
 export PALETTE_CYAN='\e[36m'
 export PALETTE_LIGHTGRAY='\e[37m'
-# Bold Text
-export PALETTE_BLACK_B='\033[1;30m'
-export PALETTE_RED_B='\e[31;1,42m'
-export PALETTE_GREEN_B='\033[32;1m'
-export PALETTE_BROWN_B='\e[1;33m'
-export PALETTE_BLUE_B='\e[1;34m'
-export PALETTE_PURPLE_B='\e[1,35m'
-export PALETTE_CYAN_B='\e[30;48;5m '
-export PALETTE_LIGHTGRAY_B='\e[1:37m'
-# Underlined Text
-export PALETTE_BLACK_B='\e[1;30m'
-export PALETTE_RED_B='\e[31;1m'
-export PALETTE_GREEN_B='\e[1;32m'
-export PALETTE_BROWN_B='\e[1;33m'
-export PALETTE_BLUE_B='\e[1;34m'
-export PALETTE_PURPLE_B='\e[1,35m'
-export PALETTE_CYAN_B='\e[1;36m'
-export PALETTE_LIGHTGRAY_B='\e[1:37m'
+
+# Background Color
+export PALETTE_BLACK_U='\e[40m'
+export PALETTE_RED_U='\e[41m'
+export PALETTE_GREEN_U='\e[42m'
+export PALETTE_BROWN_U='\e[43m'
+export PALETTE_BLUE_U='\e[44m'
+export PALETTE_PURPLE_U='\e[45m'
+export PALETTE_CYAN_U='\e[46m'
+export PALETTE_LIGHTGRAY_U='\e[47m'
+
 
 
--- test.sh
+++ test.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 source bash-palette.sh
-echo -e ${PALETTE_BLU}Colore BLUE
-echo -e ${PALETTE_RED_B}Colore RED Bold
-echo -e ${PALETTE_GREEN_B}Colore GREEN
-echo -e ${PALETTE_CYAN_B}Colore CYAN BOLD
-echo -e ${PALETTE_RESET}Colore NORMAL
-
-
-
+echo -e ${PALETTE_BLUE}Colore BLUE${PALETTE_RESET}
+echo -e ${PALETTE_UNDERLINED}${PALETTE_RED}Colore Red Underlined${PALETTE_RESET}
+echo -e ${PALETTE_CYAN}${PALETTE_BLINK}Colore CYAN NORMAL${PALETTE_RESET}
+echo -e ${PALETTE_REVERSE}${PALETTE_CYAN}${PALETTE_BOLD}Colore reversed CYAN BOLD${PALETTE_RESET}
+echo -e ${PALETTE_PURPLE_U}ColoRE BACKGORUND pURPLE ${PALETTE_RESET}
+                                                                                        
+~                                              
